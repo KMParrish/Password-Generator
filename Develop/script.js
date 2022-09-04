32df6772fd5128bd458ef0a97ca46657291e9f58
Kyle Parrish <110362534+KMParrish@users.noreply.github.com>
2022-09-04 09:41:09 -0500

    code now presents fully random outcomes

--- Develop/script.js
+++ Develop/script.js
@@ -1,52 +1,59 @@
 // Assignment code here
 var generateBtn = document.querySelector("#generate");
-var passwordCharacters = "";
 var numberCase = "0123456789";
 var specialCase = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
 var upperCaseF = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var lowerCaseF = "abcdefghijklmnopqrstuvwxyz";
-var brac = [];
 function generatePassword() {
+  var characters = [];
+  var passwordCharacters = "";
   // Prompt user with password criteria //
   var length = prompt ("How many characters would you like? Your password must be between 8 and 128 characters")
   console.log(length);
-  var Lower = confirm ("Would you like lower case letters in your password?")
-  console.log(Lower)
-  var Upper = confirm ("Would you like upper case letters in your password?")
-  console.log(Upper)
-  var Numbers = confirm ("Would you like numbers in your password?")
-  console.log(Numbers)
-  var Specialcharacters = confirm ("Would you like special characters in your password?")
-  console.log(Specialcharacters)
+  var lower = confirm ("Would you like lower case letters in your password?")
+  console.log(lower)
+  var upper = confirm ("Would you like upper case letters in your password?")
+  console.log(upper)
+  var numbers = confirm ("Would you like numbers in your password?")
+  console.log(numbers)
+  var specialCharacters = confirm ("Would you like special characters in your password?")
+  console.log(specialCharacters)
   // Validation Input//
   if (length < 8 || length > 128) {
     return generatePassword()
   }
-  if (Lower == true) {
+  if (lower == true) {
     for (var i = 0; i < lowerCaseF.length; i++){
-      brac.push(lowerCaseF[i]);
+      characters.push(lowerCaseF[i]);
     }
   }
-  if (Upper == true) {
+  if (upper == true) {
     for (var i = 0; i < upperCaseF.length; i++){
-      brac.push(upperCaseF[i]);
+      characters.push(upperCaseF[i]);
     }
   }
-  if (Numbers == true) {
+  if (numbers == true) {
     for (var i = 0; i < numberCase.length; i++){
-      brac.push(numberCase[i]);
+      characters.push(numberCase[i]);
     }
   }
-  if (Specialcharacters == true) {
+  if (specialCharacters == true) {
     for (var i = 0; i < specialCase.length; i++){
-      brac.push(specialCase[i]);
+      characters.push(specialCase[i]);
     }
   }
   // Generate Password with given criteria //
-  console.log(brac)
+  console.log(characters)
   for (let i = 0; i < length; i++) {
-    passwordCharacters = passwordCharacters + brac[Math.floor(Math.random() * brac.length)];
+    var randomNumber = Math.floor(Math.random() * characters.length)
+    var randomCharacter = characters[randomNumber]
+    passwordCharacters = passwordCharacters + randomCharacter
+    console.log(randomNumber)
+   
+    
   }
+
+  
 
   return passwordCharacters;
 }
